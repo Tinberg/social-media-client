ddd007cf9fee49b916f84728ec001666ec2462d0
mathias <tinberg92@hotmail.com>
2024-04-09 13:21:19 +0200

    Updated cypress testing after including bootstrap with npm run build. changes for the login modal, and change the -logoutBtn test to log in and out insted of relying on localStorage was made

--- cypress/e2e/loginForm.cy.js
+++ cypress/e2e/loginForm.cy.js
@@ -2,30 +2,36 @@
 describe("Login Functionality", () => {
   it("allows the user to log in with valid credentials", () => {
     cy.visit("http://127.0.0.1:5500/");
+    cy.wait(1000);
 
-    cy.get("#loginForm").within(() => {
-      cy.get('input[type="email"]').type("user@example.com");
-      cy.get('input[type="password"]').type("password123");
-      cy.root().submit();
-    });
+    cy.get('.modal-footer button[data-auth="login"]').click();
 
-    cy.get("#logout").should("exist");
+    cy.get("#loginEmail").invoke("val", "example@hotmail.com");
+    cy.get("#loginPassword").invoke("val", "example123");
+    cy.wait(1000);
+
+    cy.get("#loginForm button").contains("Login").click();
+    cy.wait(1000);
+
+    cy.get("button.btn-outline-warning").contains("Logout").should("exist");
   });
 });
 
-//cannot login with invalid credentials.
-
+// Cannot login with invalid credentials.
 describe("Invalid Login Handling", () => {
   it("displays an alert message for invalid login attempts", () => {
     cy.visit("http://127.0.0.1:5500/");
+    cy.wait(1000);
 
-    cy.get("#loginForm").within(() => {
-      cy.get('input[type="email"]').type("invalid@example.com");
-      cy.get('input[type="password"]').type("incorrect");
-      cy.root().submit();
-    });
+    cy.get('.modal-footer button[data-auth="login"]').click();
 
-    // Handle the alert dialog
+    cy.get("#loginEmail").invoke("val", "noEmail@example.com");
+    cy.get("#loginPassword").invoke("val", "Notcorrect");
+    cy.wait(1000);
+
+    cy.get("#loginForm button").contains("Login").click();
+    cy.wait(1000);
+
     cy.on("window:alert", (text) => {
       expect(text).to.contains(
         "Either your username was not found or your password is incorrect",
@@ -33,3 +39,40 @@ describe("Invalid Login Handling", () => {
     });
   });
 });
+
+//-------- without bootstrap ---------//
+// //Login with  valid credentials
+// describe("Login Functionality", () => {
+//   it("allows the user to log in with valid credentials", () => {
+//     cy.visit("http://127.0.0.1:5500/");
+
+//     cy.get("#loginForm").within(() => {
+//       cy.get('input[type="email"]').type("user@example.com");
+//       cy.get('input[type="password"]').type("password123");
+//       cy.root().submit();
+//     });
+
+//     cy.get("#logout").should("exist");
+//   });
+// });
+
+// //cannot login with invalid credentials.
+
+// describe("Invalid Login Handling", () => {
+//   it("displays an alert message for invalid login attempts", () => {
+//     cy.visit("http://127.0.0.1:5500/");
+
+//     cy.get("#loginForm").within(() => {
+//       cy.get('input[type="email"]').type("invalid@example.com");
+//       cy.get('input[type="password"]').type("incorrect");
+//       cy.root().submit();
+//     });
+
+//     // Handle the alert dialog
+//     cy.on("window:alert", (text) => {
+//       expect(text).to.contains(
+//         "Either your username was not found or your password is incorrect"
+//       );
+//     });
+//   });
+// });
--- cypress/e2e/logoutBtn.cy.js
+++ cypress/e2e/logoutBtn.cy.js
@@ -1,16 +1,38 @@
-describe("Logout Functionality", () => {
-  it("successfully logs out the user and clears browser storage", () => {
-    cy.window().then((win) => {
-      win.localStorage.setItem("token", "testToken123");
-    });
-
-    //(This is the hardcoded url with my username. just for testin purposes)
+//---This test logs in a user, performs a logout, and verifies that the login button exists after logging out
+describe("Logout function", () => {
+  it("Logs in the user and log out.", () => {
+    //-- Visit page
     cy.visit("/?view=profile&name=Tinberg92");
+    cy.wait(1000);
+    cy.get('.modal-footer button[data-auth="login"]').click();
 
-    cy.window().its("localStorage.token").should("equal", "testToken123");
-
-    cy.get("button#logout").click();
-
-    cy.window().its("localStorage.token").should("not.exist");
+    //-- Login
+    cy.get("#loginEmail").invoke("val", "mattin51583@stud.noroff.no");
+    cy.get("#loginPassword").invoke("val", "detvarengang");
+    cy.wait(1000);
+    cy.get("#loginForm button").contains("Login").click();
+    cy.wait(1000);
+    //--logout
+    cy.get('button[data-auth="logout"]').click();
+    //--login exist = logout successfully
+    cy.get('.modal-footer button[data-auth="login"]').should("exist");
   });
 });
+
+// //---------- without bootstrap ----------//
+// describe("Logout Functionality", () => {
+//   it("successfully logs out the user and clears browser storage", () => {
+//     cy.window().then((win) => {
+//       win.localStorage.setItem("token", "testToken123");
+//     });
+
+//     //(This is the hardcoded url with my username. just for testin purposes)
+//     cy.visit("/?view=profile&name=Tinberg92");
+
+//     cy.window().its("localStorage.token").should("equal", "testToken123");
+
+//     cy.get("button#logout").click();
+
+//     cy.window().its("localStorage.token").should("not.exist");
+//   });
+// });
